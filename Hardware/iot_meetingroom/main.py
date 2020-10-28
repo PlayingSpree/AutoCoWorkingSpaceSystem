e18ae464c18f5bdd81b819df1fa1f06020fd7a81
Natchapon Santiphiboon <winingame@hotmail.com>
2020-10-28 18:56:27 +0700

    Hardware: Detailed error response

--- Hardware/iot_meetingroom/main.py
+++ Hardware/iot_meetingroom/main.py
@@ -17,14 +17,14 @@ def index():
 @app.route('/<int:thing_id>/', methods=['PUT'])
 def update_thing_data(thing_id):
     if not request.json:
-        abort(400)
+        return {'iot_error': 'Not JSON.'}, 400
     thing = next((thing for thing in setting.available_things if thing.id == thing_id), None)
     if thing is None:
-        abort(404)
+        return {'iot_error': 'Thing not found.'}, 404
     if thing.update(request.json):
         return thing.data
     else:
-        abort(400)
+        return {'iot_error': 'Invalid data.'}, 400
 
 
 app.run(host='0.0.0.0', debug=True)
