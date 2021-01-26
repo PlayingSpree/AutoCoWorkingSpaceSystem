18ff974d3eb569feceb3c333da719ef31c34a062
PlayingSpree <winingame@hotmail.com>
2021-01-26 15:11:37 +0700

    Mobile: Reset Project

---
+++ Mobile/acws_app/android/app/src/main/kotlin/kmitl/acws_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package kmitl.acws_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- Mobile/auto_cws_app/android/app/src/main/kotlin/com/kmitl/project/auto_cws_app/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.kmitl.project.auto_cws_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
