a23e1ff44a6d9a3078a210566f7e621a84d9e52f
PlayingSpree <winingame@hotmail.com>
2021-03-13 15:03:36 +0700

    Add Install.bat + Register model to admin

--- Backend/feedback/admin.py
+++ Backend/feedback/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from feedback.models import Feedback, ProblemType, Problem
+
+admin.site.register(Feedback)
+admin.site.register(ProblemType)
+admin.site.register(Problem)
