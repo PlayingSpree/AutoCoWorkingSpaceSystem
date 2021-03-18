56a48fee7427bb9ea6223a3e176f2bc2d87be964
PlayingSpree <winingame@hotmail.com>
2021-03-18 15:18:39 +0700

    Delete Mobile/acws_app directory

--- Mobile/acws_app/lib/app_config.dart
+++
@@ -1,7 +0,0 @@
-const debugOnEmulator = true;
-const serverUrl =
-    debugOnEmulator ? 'http://10.0.2.2:8000' : 'http://127.0.0.1:8000';
-
-class AppData {
-  static String authToken;
-}
--- Mobile/acws_app/lib/app_style.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/material.dart';
-
-class AppTextStyle {
-  static const TextStyle HeaderText =
-      const TextStyle(fontSize: 20, fontWeight: FontWeight.w500);
-}
