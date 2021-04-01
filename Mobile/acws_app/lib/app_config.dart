8f47730fed9e9d78c8e8009e3d0e896efbce3b89
Natchapon Santiphiboon <winingame@hotmail.com>
2021-04-01 14:06:29 +0700

    Mobile: Set ip

--- Mobile/acws_app/lib/app_config.dart
+++ Mobile/acws_app/lib/app_config.dart
@@ -1,6 +1,6 @@
 const debugOnEmulator = true;
-const serverUrl =
-    debugOnEmulator ? 'http://10.0.2.2:8000' : 'http://127.0.0.1:8000';
+const serverUrl = 'http://192.168.137.1:8000';
+    // debugOnEmulator ? 'http://10.0.2.2:8000' : 'http://127.0.0.1:8000';
 
 class AppData {
   static String authToken;
