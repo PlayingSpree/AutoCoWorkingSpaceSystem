49b26fd1f39bcbd2927737cd082c61927e2c08cb
PlayingSpree <winingame@hotmail.com>
2021-04-02 12:08:48 +0700

    Mobile: Change Login Title

--- Mobile/acws_app/lib/page/login/login.dart
+++ Mobile/acws_app/lib/page/login/login.dart
@@ -26,7 +26,7 @@ class _LoginPageState extends State<LoginPage> {
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            Text('Login Page', style: TextStyle(fontSize: 64)),
+            Text('ACWP', style: TextStyle(fontSize: 64)),
             LoginForm()
           ],
         ),
