4cbfcd0b89639bd3288333484f57e3ba661dfc9d
PlayingSpree <winingame@hotmail.com>
2021-04-03 17:39:46 +0700

    Backend: Access Stat

--- Backend/iot/urls.py
+++ Backend/iot/urls.py
@@ -4,6 +4,7 @@ from rest_framework import routers
 from iot.views_door import IoTDoorViewSet
 from iot.views_room import IoTRoomViewSet
 from iot.views_meeting_room_iot import MeetingRoomIoTViewSet
+from iot.views_stats import AccessStat
 
 router = routers.DefaultRouter()
 router.register('room/setup', MeetingRoomIoTViewSet)
@@ -11,5 +12,6 @@ router.register('room', IoTRoomViewSet, basename='iotroom')
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('key/', IoTDoorViewSet.as_view())
+    path('key/', IoTDoorViewSet.as_view()),
+    path('stat/', AccessStat.as_view())
 ]
