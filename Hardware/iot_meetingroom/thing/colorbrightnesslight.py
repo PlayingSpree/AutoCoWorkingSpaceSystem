ebe993bf0861194cda7272aa0f6a474df1c3984a
Natchapon Santiphiboon <winingame@hotmail.com>
2021-04-04 22:21:10 +0700

    Hardware IoT: Brightness Light Control

--- Hardware/iot_meetingroom/thing/colorbrightnesslight.py
+++ Hardware/iot_meetingroom/thing/colorbrightnesslight.py
@@ -1,5 +1,5 @@
 from thing.thing import Thing
-
+from thing import remote_interface
 
 class ColorBrightnessLight(Thing):
     def __init__(self, name, description):
@@ -17,9 +17,25 @@ class ColorBrightnessLight(Thing):
         }
         data = {'power': False, 'brightness_up': False,'brightness_down': False}
         super().__init__(name, description, type, data_info, data)
+
+    def update(self, data):
+        if super().update(data):
+            if 'power' in data:
+                if data['power']:
+                    remote_interface.LED_ON()
+                else:
+                    remote_interface.LED_OFF()
+            if 'brightness_up' in data and data['brightness_up']:
+                remote_interface.LED_UP()
+            if 'brightness_down' in data and data['brightness_down']:
+                remote_interface.LED_DOWN()
+            return True
+        return False
     
     def turn_on(self):
+        self.update({'power': True})
         return True
 
     def turn_off(self):
+        self.update({'power': False})
         return True
